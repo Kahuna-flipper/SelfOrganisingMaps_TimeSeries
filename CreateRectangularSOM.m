aa49782fffbb69dcc7ad0cd9bf1da5fb7f2247b0
Kahuna-flipper <ch17btech11013@iith.ac.in>
2020-06-06 01:15:43 +0530

    Rectangular Map

--- CreateRectangularSOM.m
+++ CreateRectangularSOM.m
@@ -1,5 +1,6 @@
 
 %% Creates a rectangular mesh and initialises weights
+
 function map = CreateRectangularSOM(rows,columns,dimensions)
 elements = rows*columns;
 map = abs(randn(elements,dimensions));
