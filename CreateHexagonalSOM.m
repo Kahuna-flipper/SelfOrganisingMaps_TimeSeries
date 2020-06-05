d7be7b3d59253633787e848a00cce98365c82bc9
Kahuna-flipper <ch17btech11013@iith.ac.in>
2020-06-06 01:42:38 +0530

    added dimensinality freedom

--- CreateHexagonalSOM.m
+++ CreateHexagonalSOM.m
@@ -1,6 +1,7 @@
 %% Creates a hexagonal mesh and initialises weights
 
 
-function hexmap = CreateHexagonalSOM(elements,dimensions)
+function hexmap = CreateHexagonalSOM(rows,columns,dimensions)
+elements = rows*columns;
 hexmap = abs(randn(elements,dimensions));
 end
