8fcb79f27c8e220ff9793eec71e915510f8f531e
Kahuna-flipper <ch17btech11013@iith.ac.in>
2020-06-24 20:41:29 +0530

    Deleted unwanted files

--- trial.m
+++
@@ -1,8 +0,0 @@
-clear all;
-close all;
-clc;
-
-
-TRAIN = load('synthetic_control_TRAIN'); % Only these two lines need to be changed to test a different dataset. %
-TEST = load('synthetic_control_TEST' ); % Only these two lines need to be changed to test a different dataset. %
-
